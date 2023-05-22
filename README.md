d10799a8f7772a98d1856990b869679a154c7098
Peter Francis Robante <robantepeterfran@gmail.com>
2023-05-22 11:16:24 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# admin-dashboard
-Dashboard references UI for future USE
+For the Sidebar cool design.
+https://github.com/azouaoui-med/react-pro-sidebar
+install it: npm i react-pro-sidebar@0.7.1
+
+For the Table use the MUI grid
+https://mui.com/x/react-data-grid/
---
+++ src/scenes/dashboard/index.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
+
+const Dashboard = () => {
+  return (
+    <Box m="20px">
+      <Box>
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard!" />
+      </Box>
+    </Box>
+  );
+};
+export default Dashboard;
